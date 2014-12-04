2d6afba1e4ea9bd2100709e31e8336ebaaef89b8
abdulla16 <abdulla16@gmail.com>
2014-12-04 17:45:18 -0600

    Update YellowPagesBusinessExtractor.php

--- YellowPagesBusinessExtractor.php
+++ YellowPagesBusinessExtractor.php
@@ -40,9 +40,7 @@ class YellowPagesBusinessExtractor implements BusinessExtractorInterface {
 				foreach($organicResults[0]->find("div.result") as $searchResult) {
 					try {
 					$name = $this->extractItem($searchResult, 'name');
-					if($name == "Shari's Berries")
-					{
-						echo "Extracting business: $name...\n";
+					echo "Extracting business: $name...\n";
 					$businessBranch = new BusinessBranch();
 					$businessBranch->setStreetAddress($this->extractItem($searchResult, 'streetAddress'));
 					$businessBranch->setAddressLocality($this->extractItem($searchResult, 'addressLocality'));
@@ -69,7 +67,6 @@ class YellowPagesBusinessExtractor implements BusinessExtractorInterface {
 						$businesses[$name] = $business;
 					}
 					$this->dataManager->saveBusiness($business);
-					}
 					} catch(Exception $e) {
 						var_dump($e);
 						exit(1);
