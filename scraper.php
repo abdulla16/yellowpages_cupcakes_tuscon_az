dc69fdad16c8baefcb906e970c8ddc64d20cf561
abdulla16 <abdulla16@gmail.com>
2014-12-04 18:01:31 -0600

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -17,8 +17,8 @@ $extractor = new YellowPagesBusinessExtractor(function ($url) { return scraperwi
 	new DataManager());
 $businesses = $extractor->extractAndSaveBusinesses('Tuscon, AZ', 'cupcakes');
 $dataManager = new DataManager();
-foreach($businesses as $business) {
-	$dataManager->saveBusiness($business);
-}
+//foreach($businesses as $business) {
+//	$dataManager->saveBusiness($business);
+//}
 
 exit(0);
